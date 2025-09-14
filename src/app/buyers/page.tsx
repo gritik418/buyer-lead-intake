9b4328cf48696012d2442487f99236566e3b2e1b
Ritik <160255700+gritik418@users.noreply.github.com>
2025-09-14 16:41:21 +0530

    feat: add filters in buyers list page

--- src/app/buyers/page.tsx
+++ src/app/buyers/page.tsx
@@ -12,6 +12,7 @@ interface QueryParams {
   status?: Status;
   timeline?: Timeline;
   page?: string;
+  sort?: string;
 }
 
 export const revalidate = 0;
@@ -40,12 +41,23 @@ export default async function BuyersPage({
   if (searchParams.status) where.status = searchParams.status;
   if (searchParams.timeline) where.timeline = searchParams.timeline;
 
+  let orderBy: any = { updatedAt: "desc" };
+
+  if (searchParams?.sort) {
+    const keyValue = searchParams.sort.split(":");
+    if (keyValue.length === 2) {
+      orderBy = {
+        [keyValue[0]]: keyValue[1],
+      };
+    }
+  }
+
   const [buyers, total] = await Promise.all([
     prisma.buyer.findMany({
       where,
       skip,
       take: pageSize,
-      orderBy: { updatedAt: "desc" },
+      orderBy: orderBy,
     }),
     prisma.buyer.count({ where }),
   ]);
@@ -69,7 +81,79 @@ export default async function BuyersPage({
 
         <SearchBar />
 
-        <table className="w-full mt-6 border-collapse border">
+        <form className="flex flex-wrap gap-4 my-6" action={"/buyers"}>
+          <select
+            name="city"
+            defaultValue={searchParams.city || ""}
+            className="bg-gray-800 border border-gray-600 rounded px-2 py-1"
+          >
+            <option value="">All Cities</option>
+            {Object.values(City).map((c) => (
+              <option key={c} value={c}>
+                {c}
+              </option>
+            ))}
+          </select>
+
+          <select
+            name="propertyType"
+            defaultValue={searchParams.propertyType || ""}
+            className="bg-gray-800 border border-gray-600 rounded px-2 py-1"
+          >
+            <option value="">All Property Types</option>
+            {Object.values(PropertyType).map((p) => (
+              <option key={p} value={p}>
+                {p}
+              </option>
+            ))}
+          </select>
+
+          <select
+            name="status"
+            defaultValue={searchParams.status || ""}
+            className="bg-gray-800 border border-gray-600 rounded px-2 py-1"
+          >
+            <option value="">All Status</option>
+            {Object.values(Status).map((s) => (
+              <option key={s} value={s}>
+                {s}
+              </option>
+            ))}
+          </select>
+
+          <select
+            name="timeline"
+            defaultValue={searchParams.timeline || ""}
+            className="bg-gray-800 border border-gray-600 rounded px-2 py-1"
+          >
+            <option value="">All Timelines</option>
+            {Object.values(Timeline).map((t) => (
+              <option key={t} value={t}>
+                {t}
+              </option>
+            ))}
+          </select>
+
+          <select
+            name="sort"
+            defaultValue={searchParams.sort || "updatedAt:desc"}
+            className="bg-gray-800 border border-gray-600 rounded px-2 py-1"
+          >
+            <option value="updatedAt:desc">Updated (newest first)</option>
+            <option value="updatedAt:asc">Updated (oldest first)</option>
+            <option value="fullName:asc">{"Name (A - Z)"}</option>
+            <option value="fullName:desc">{"Name (Z - A)"}</option>
+          </select>
+
+          <button
+            type="submit"
+            className="bg-indigo-500 cursor-pointer px-4 py-1 rounded text-white"
+          >
+            Apply
+          </button>
+        </form>
+
+        <table className="w-full mt-2 border-collapse border">
           <thead>
             <tr className="bg-gray-500">
               <th className="border px-2 py-1">Name</th>
@@ -106,7 +190,7 @@ export default async function BuyersPage({
                 <td className="border px-2 py-1">
                   <a
                     href={`/buyers/${b.id}`}
-                    className="text-blue-600 hover:underline"
+                    className="text-indigo-400 font-semibold hover:underline"
                   >
                     View / Edit
                   </a>
